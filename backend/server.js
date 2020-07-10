92bb926dd58cadf491ff66903550cd1758a5a39c
pliffisson <pliffissongomes@gmail.com>
2020-07-10 15:42:52 -0400

    Configurando o nunjucks

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const nunjkucks = require("nunjucks");
+
+// Chamar o servidor express
+server = express();
+
+// Entregando os arquivos estaticos no express
+server.use(express.static("public"));
+server.set("view engine", "njk");
+
+// Configurar o template engine
+nunjkucks.configure("views", {
+  express: server,
+  autoescape: false,
+  noCache: true,
+});
+
+//Criando as rotas do servidor express
+server.get("/", function (req, res) {
+  return res.render("index");
+});
+
+server.get("/sobre", function (req, res) {
+  return res.render("sobre");
+});
+
+server.get("/receita", function (req, res) {
+  return res.render("receita");
+});
+
+//Configurar a saida do servidor
+server.listen(3005, function () {
+  console.log("Servidor esta em execução");
+});
